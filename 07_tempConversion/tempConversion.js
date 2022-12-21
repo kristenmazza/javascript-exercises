2726f5e2230d48b645133e5dc3859ca23c3c5850
kristenmazza <kbmazza@gmail.com>
2022-12-20 17:17:27 -0800

    Add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+let temp;
+let newTemp;
+
+const convertToCelsius = function(temp) {
+  newTemp = (temp - 32) / 1.8;
+  if (Number.isInteger(newTemp)) return newTemp;
+  else {
+    return Math.round(newTemp*10)/10;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  newTemp = (temp * 1.8) + 32;
+  if (Number.isInteger(newTemp)) return newTemp;
+  else {
+    return Math.round(newTemp*10)/10;
+  }
 };
 
 // Do not edit below this line
