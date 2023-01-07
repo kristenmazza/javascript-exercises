e31beac3860890c0447202244cfe3f2ab3391d87
Kristen Mazza <kbmazza@gmail.com>
2023-01-07 15:32:05 -0800

    Include solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,31 @@
 const fibonacci = function(item) {
-    const sequence = [0, 1];
-
     if (item < 0) return "OOPS";
+    if (item === 0) return 0;
 
-    let nextNum = 0;
-    for (i=1; i<item; i++) {
-        nextNum = sequence[i] + sequence[i-1];
-        sequence.push(nextNum);
+    let a = 0;
+    let b = 1;
+
+    for (let i=1; i<item; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
     }
+    return b;
+}
 
-    return sequence[item];
-};
+// const fibonacci = function(item) {
+//     const sequence = [0, 1];
+
+//     if (item < 0) return "OOPS";
+
+//     let nextNum = 0;
+//     for (let i=1; i<item; i++) {
+//         nextNum = sequence[i] + sequence[i-1];
+//         sequence.push(nextNum);
+//     }
+
+//     return sequence[item];
+// };
 
 // Do not edit below this line
 module.exports = fibonacci;
