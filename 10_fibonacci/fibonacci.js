299453891bd881a0b7135a7ae6ea4f75f66708a5
Kristen Mazza <kbmazza@gmail.com>
2023-01-07 15:14:57 -0800

    Insert solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(item) {
+    const sequence = [0, 1];
 
+    if (item < 0) return "OOPS";
+
+    let nextNum = 0;
+    for (i=1; i<item; i++) {
+        nextNum = sequence[i] + sequence[i-1];
+        sequence.push(nextNum);
+    }
+
+    return sequence[item];
 };
 
 // Do not edit below this line
