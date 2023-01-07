97a2d2c0239d5bbe8b50beb8568d27de3a984d3c
Kristen Mazza <kbmazza@gmail.com>
2023-01-07 14:47:47 -0800

    Complete exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((total, currentValue) => total + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((a, b) => a*b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n===0) return 1;
+  let total = 1;
+  for (let i = n; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, '');
+    return (
+        processedString
+                .split("")
+                .reverse()
+                .join("") == processedString
+    );
 };
+
+// const palindromes = function (text) {   
+//     text = text.replace(/[^0-9a-z]/gi, '');
+//     text = text.toLowerCase();
+//     let chars = text.split('');
+//     let newText = "";
+
+//     for (let i = (text.length-1); i >= 0; i--) {
+//         newText += text[i];
+//     }
+
+//     return newText === text;
+    
+// };
 
 // Do not edit below this line
 module.exports = palindromes;
